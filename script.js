86e76db9a808e2eb0c0891dca4cae5f47bb96d3a
Thatchidinma <ndukubachidinma@gmail.com>
2023-09-07 17:25:36 +0100

    made changes in html and css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const currentDate = new Date();
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const dayIndex = currentDate.getDay();
 const dayName = daysOfWeek[dayIndex];
-document.getElementById('day').textContent = "day of the week: " + dayName;
+document.getElementById('day').textContent = "Day of the week: " + dayName;
 
 function updateUTCTime() {
     const currentUTC = new Date().getTime();
